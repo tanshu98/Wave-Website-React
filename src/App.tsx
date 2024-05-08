bf2cc5908693b0000048737233b3b56e9cca361d
Tanshu Allewar <tanshuallewar98@gmail.com>
2024-05-08 12:07:52 +0530

    Added Routing and multiple components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import Services from './Components/Services/Services';
+import Portfolio from './Components/Portfolio/Portfolio';
+import About from './Components/About/About';
+import Team from './Components/Team/Team';
+import Pricing from './Components/Pricing/Pricing';
+import Blog from './Components/Blog/Blog';
+import Contact from './Components/Contact/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <Router>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/services' element={<Services />} />
+      <Route path='/portfolio' element={<Portfolio />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/team' element={<Team />} />
+      <Route path='/pricing' element={<Pricing />} />
+      <Route path='/blog' element={<Blog />} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
+   </Router>
+  )
 }
 
-export default App;
+export default App
