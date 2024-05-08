fff265b00a9de47308b7b635b2d0de2839d63f53
Tanshu Allewar <tanshuallewar98@gmail.com>
2024-05-08 19:34:40 +0530

    Added Services Component with Responsive Styling

--- src/Components/Services/Services.tsx
+++ src/Components/Services/Services.tsx
@@ -1,9 +1,150 @@
-import React from 'react'
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
+import { ServicesStyles } from "./ServicesStyles";
+import cardSvg from "../../assets/card svg.svg";
+import aeroplaneSvg from "../../assets/aeroplane svg.svg";
+import mercedesSvg from "../../assets/mercedes svg.svg";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import clock from '../../assets/clock svg.svg';
 
 function Services() {
   return (
-    <div>Services</div>
-  )
+    <>
+      <Box sx={ServicesStyles.mainContainer}>
+        <Box sx={ServicesStyles.textContainer}>
+          <Typography sx={ServicesStyles.oswaldFontContainer} variant="h2">
+            You will get the
+          </Typography>
+          <Typography sx={ServicesStyles.oswaldFontContainer} variant="h2">
+            perfect{" "}
+            <span style={ServicesStyles.satisyFontContainer}>resolutions</span>{" "}
+            with{" "}
+          </Typography>
+          <Typography variant="h2" sx={ServicesStyles.oswaldFontContainer}>
+            our proficient services.{" "}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            marginLeft: "auto",
+            marginRight: "auto",
+            gap: 6,
+            mt: 6,
+            mb: 6,
+            flexWrap: 'wrap',
+            textAlign: 'center'
+          }}
+        >
+          <Box>
+            <img
+              style={ServicesStyles.mercedesImg}
+              src={mercedesSvg}
+              alt="Market Research"
+            />
+            <Box sx={ServicesStyles.servicesTextContainer}>
+              <Typography
+                variant="h4"
+                sx={ServicesStyles.servicesHeadingText}
+              >
+                Market Research
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={ServicesStyles.servicesPara}
+              >
+                We can help you to <br /> understand your target <br /> market
+                and identify new opportunities for <br /> growth. We offer a
+                variety of market research <br /> services, interviews, and
+                focus groups.
+              </Typography>
+              <Box sx={ServicesStyles.learnMoreContainer}>
+              <Button sx={ServicesStyles.learnMoreButton} >Learn More  </Button>
+              <ArrowForwardIcon sx={ServicesStyles.arrowButton} />
+              </Box>
+            </Box>
+          </Box>
+          <Box>
+            <img
+              style={ServicesStyles.cardImg}
+              src={cardSvg}
+              alt="Web Design"
+            />
+            <Box sx={ServicesStyles.servicesTextContainer}>
+              <Typography
+                variant="h4"
+                sx={ServicesStyles.servicesHeadingText}
+              >
+                Web Design
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={ServicesStyles.servicesPara}
+              >
+                We can help you to create a visually<br /> appealing and user-friendly website.<br /> We take into account your brand <br /> identity and target audience when <br /> designing your website.
+              </Typography>
+              <Box sx={ServicesStyles.learnMoreContainer}>
+              <Button sx={ServicesStyles.learnMoreButton} >Learn More  </Button>
+              <ArrowForwardIcon sx={ServicesStyles.arrowButton} />
+              </Box>
+            </Box>
+          </Box>
+          <Box>
+            <img
+              style={ServicesStyles.aeroplaneImg}
+              src={aeroplaneSvg}
+              alt="SEO Services"
+            />
+            <Box sx={ServicesStyles.servicesTextContainer}>
+              <Typography
+                variant="h4"
+                sx={ServicesStyles.servicesHeadingText}
+              >
+                SEO Services
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={ServicesStyles.servicesPara}
+              >
+                We can help you to improve your <br /> website's visibility in search engine<br />results pages (SERPs). This can lead to<br /> more traffic to your website and more <br /> conversions.
+              </Typography>
+              <Box sx={ServicesStyles.learnMoreContainer}>
+              <Button sx={ServicesStyles.learnMoreButton} >Learn More  </Button>
+              <ArrowForwardIcon sx={ServicesStyles.arrowButton} />
+              </Box>
+            </Box>
+          </Box>
+          <Box>
+            <img
+              style={ServicesStyles.clockImg}
+              src={clock}
+              alt="Content Marketing
+              "
+            />
+            <Box sx={ServicesStyles.servicesTextContainer}>
+              <Typography
+                variant="h4"
+                sx={ServicesStyles.servicesHeadingText}
+              >
+               Content Marketing
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={ServicesStyles.servicesPara}
+              >
+                We can help you to promote your <br /> business online through a variety of<br />digital marketing channels, including<br /> SEO, PPC, social media marketing, and <br /> email marketing.
+              </Typography>
+              <Box sx={ServicesStyles.learnMoreContainer}>
+              <Button sx={ServicesStyles.learnMoreButton} >Learn More  </Button>
+              <ArrowForwardIcon sx={ServicesStyles.arrowButton} />
+              </Box>
+            </Box>
+          </Box>
+          
+        </Box>
+      </Box>
+    </>
+  );
 }
 
-export default Services
+export default Services;
---
+++ src/Components/Services/ServicesStyles.ts
@@ -0,0 +1,74 @@
+export const ServicesStyles = {
+  mainContainer: {
+    display: "flex",
+    pb: 6,
+    pt: 6,
+    flexDirection: "column",
+    gap: 3,
+  },
+  textContainer: {
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "center",
+    gap: 2,
+  },
+  oswaldFontContainer: {
+    fontFamily: "Oswald",
+    fontWeight: 600,
+  },
+  satisyFontContainer: {
+    fontFamily: "satisfy",
+    fontWeight: 400,
+    color: "#a30000",
+    // textDecoration: "underline #b8e1ff 10px",
+  },
+  servicesOffered: {},
+  mercedesImg: {
+    backgroundColor: "#f2e9ac",
+    padding: "12px",
+    borderRadius: "5px",
+  },
+  cardImg: {
+    backgroundColor: "#caf0c7",
+    padding: "12px",
+    borderRadius: "5px",
+  },
+  aeroplaneImg: {
+    backgroundColor: "#ffe9f3",
+    padding: "12px",
+    borderRadius: "5px",
+  },
+  clockImg: {
+    backgroundColor: "#cdf7f6",
+    padding: "12px",
+    borderRadius: "5px",
+  },
+
+  servicesTextContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 2,
+  },
+  servicesHeadingText: {
+    fontFamily: "Oswald",
+    mt: 2,
+    fontWeight: 500,
+  },
+  servicesPara: {
+    color: "#6c7781",
+    fontWeight: 500,
+    lineHeight: 1.5,
+  },
+  learnMoreContainer: {
+    display: "flex",
+    alignItems: "center",
+    color: "#818ac5",
+  },
+  learnMoreButton: {
+    color: "#2f3fa8",
+    fontWeight: 700,
+  },
+  arrowButton: {
+    fontWeight: 700,
+  },
+};
