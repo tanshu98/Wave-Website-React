4c7f1237ec3083d20b827198aab8bdfc852b814f
Tanshu Allewar <tanshuallewar98@gmail.com>
2024-05-08 19:33:22 +0530

    Added Header and Services Component inside Home Component

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Header from '../Header/Header'
+import Services from '../Services/Services'
 
 
 function Home() {
   return (
     <>
-    Home
+    <Header />
+    <Services />
     </>
   )
 }
